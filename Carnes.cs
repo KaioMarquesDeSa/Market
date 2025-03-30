a4bf50d6da2c95d3f4cf9ad694431e9b191027bf
Kaio <leydi.centerclock13@gmail.com>
2025-03-30 17:38:49 -0300

    Add files via upload

---
+++ Carnes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermercado
+{
+    public class Carnes
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+
+        public Carnes(string nome, decimal preco, int quantidadeEmEstoque)
+        {
+            Nome = nome;
+            Preco = preco;
+            QuantidadeEmEstoque = quantidadeEmEstoque;
+        }
+    }
+}
